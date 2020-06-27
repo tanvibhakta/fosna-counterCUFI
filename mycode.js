2816cdfbb4a8a42671856634ca8efbe302535938
Tanvi Bhakta <tanvibhakta@gmail.com>
2020-06-27 18:14:27 +0530

    Remove unused functions + comments

--- mycode.js
+++ mycode.js
@@ -29,26 +29,6 @@ function nextBlock(item) {
 }
 
 /**
- * This function closes the image present in the content of the item passed,
- * as well as the following items, until it reaches the next header
- * @param item Any node inside a block
- * @param headers A selector that is a string of headers in descending order of priority
- *                that the function looks for to find a closing case
- */
-function toggleImg(item, headers) {
-
-    //Any image that comes after it
-    //Fixme: give me a reasonable variable name?
-    let nb = nextBlock(item).slideToggle();
-
-    //Paragraphs in the next block until the next heading
-    nb = nextBlock(nb).children().children().first();
-    nb.slideToggle();
-    nb.nextUntil(headers).slideToggle();
-    // TODO: toggle margins of said hidden image div
-}
-
-/**
  * This function takes a header and returns a list of headers that the blocks
  * need to toggle items until, sorted in decreasing order of priority
  * @param header A node
@@ -70,7 +50,7 @@ function constructHeaderSelector(header) {
  */
 function toggle(header) {
 
-    // header
+    // Algorithm: header
     //     - Check where the next header is
     //         - toggle all the nodes under it until header
     //         - if next header not reached, skip to the next block
@@ -79,7 +59,7 @@ function toggle(header) {
     let toggleables = [];
     let headerSelector = constructHeaderSelector(header);
 
-    if($(header).nextAll(headerSelector).length) {
+    if ($(header).nextAll(headerSelector).length) {
         // We have a sibling in our block that's an appropriate header
         // We just add these siblings to our toggleables list, and call it done
         toggleables = [$(header).nextUntil(headerSelector)];
@@ -116,8 +96,6 @@ function toggle(header) {
 
         }
     }
-    console.log(toggleables);
-
 
     toggleables.forEach((t) => {
         if ($(header).hasClass('ui-state-active')) {
@@ -129,127 +107,28 @@ function toggle(header) {
     });
 
     $(header).toggleClass("ui-state-active");
-
-// while ($(item).nextUntil(headers).length == 0) {
-//     $(item).nextAll(headers).slideUp();
-// }
-//
-// //Any image that comes after it
-// //Fixme: give me a reasonable variable name?
-// let nb = nextBlock(item).slideToggle();
-//
-// //Paragraphs in the next block until the next heading
-// nb = nextBlock(nb).children().children().first();
-// nb.slideToggle();
-// nb.nextUntil(headers).slideToggle();
-// // TODO: toggle margins of said hidden image div
-//
-
-}
-
-function closeH4() {
-
-    $(this).nextUntil("H4, H3, H2, H1").slideUp();
-    $(this).removeClass("ui-state-active");
-
-    //no end in current block implies there is a new block with image
-    if ($(this).nextAll("H4, H3, H2, H1").length == 0) {
-        toggleImg(this, "H4, H3, H2, H1");
-    }
-}
-
-
-function closeH3() {
-
-    //ensure all h4s are closed
-    closeH4()
-    $(this).nextUntil("H3, H2, H1").slideUp();
-    $(this).removeClass("ui-state-active");
-
-    let current = this
-    console.log("1", current);
-    //no end right there
-    while ($(current).length != 0 && $(current).nextAll("H3, H2, H1").length == 0) {
-
-        console.log("2", current);
-        //there is an image
-        if (nextBlock(current).has("figure")) {
-            toggleImg(current, "H3, H2, H1");
-            current = $(current).next();
-            console.log("3", current);
-        }
-        if (current.has("H3, H2, H1").length == 0) {
-            $(current).children().slideUp();
-        } else {
-            $(current).children().children().first().nextUntil("H3, H2, H1").slideUp();
-        }
-        current = $(current).next();
-        console.log("4", current);
-
-    }
-}
-
-/* Toggle state of clicked H4 */
-function toggleH4() {
-    $(this).nextUntil("H4, H3, H2, H1").slideToggle();
-    $(this).toggleClass("ui-state-active");
-
-    //no end in current block implies there is a new block with image
-    if ($(this).nextAll("H4, H3, H2, H1").length == 0)
-        toggleImg(this, "H4, H3, H2, H1")
 }
 
 
 $(document).ready(function () {
 
-    /* Setup cursors to show over H1, H2, H3, H4 */
-    /* Appending the FA icon to every heading */
+    // Setup cursors to show over H1, H2, H3, H4
+    // Appending the FA icon to every heading
     $(all).prepend("<i />").css("cursor", "pointer");
     $("i").addClass(icon, "ui-state-active");
 
-    /* Remove the arrow from the last link TODO: hardcoded, fix */
+    // Remove the arrow from the last link TODO: hardcoded, fix
     $(".html-block .sqs-block-content H2 i").last().removeClass(icon, "ui-state-active");
 
-    /* Close all tabs to begin with */
-    // $(IMG).slideUp();
-    // $(H1).nextUntil("H1").slideUp();
-    // $(H2).nextUntil("H2, H1").slideUp();
-    // $(H3).nextUntil("H3, H2, H1").slideUp();
-    // $(H4).nextUntil("H4, H3, H2, H1").slideUp();
-    // $(H3).each(closeH3);
-    // $(H4).each(closeH4);
-    // var b = $(IMG).parentsUntil(".sqs-block").parent().next().children().children().first();
-    // b.slideToggle();
-    // b.nextUntil("H4, H3, H2, H1").slideToggle();
-
-    //Open H3 subheadings as well
-    // $(H3).slideDown();
-
-    // /* Toggle state of clicked H2 */
-    // $(H2).click(function () {
-    //     $(this).nextUntil("H4, H3, H2, H1").slideToggle();
-    //     $(this).toggleClass("ui-state-active");
-    // });
-
-    // /* Toggle state of clicked H3 */
-    // $(H3).click(function () {
-    //     $(this).nextUntil("H4, H3, H2, H1").slideToggle();
-    //     $(this).toggleClass("ui-state-active");
-
-    //     //Toggle appearance of H4
-    //     $(this).nextUntil("H3").next("H4").slideToggle();
-    // });
-
-    // $(H3).click(closeH3);
-    // $(H4).click(toggle);
-
-    $('[data-section-id="5ee2523e3dba006e9becb097"]').find('h1, h2, h3, h4, h5').click(function() {
+    $('[data-section-id="5ee2523e3dba006e9becb097"]').find('h1, h2, h3, h4, h5').click(function () {
         toggle(this)
     });
-    $([H2, H4, H3].join(', ')).each(function() {
+
+    $([H2, H4, H3].join(', ')).each(function () {
         $(this).addClass("ui-state-active");
     })
-    $(H4).each(function() {
+    
+    $(H4).each(function () {
         toggle(this);
     })
 
